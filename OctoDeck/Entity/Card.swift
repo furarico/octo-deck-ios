7ce03c6f8383c0dc20abe24ba7c46b89924beeb8
ryka-1104 <gyd90621@gmail.com>
2025-12-14 18:39:29 +0900

    make Card.swift

---
+++ OctoDeck/Entity/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  OctoDeck
+//
+//  Created by 藤間里緒香 on 2025/12/14.
+//
+
+struct Card: Identifiable, Equatable {
+    let id: Int
+    let useName: String
+    let fullName: String
+    let iconUrl: String
+}
+
