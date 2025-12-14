6a6aea069708c8746d3f3f903f9acff1c4324446
Kanta Oikawa <iam@kantacky.com>
2025-12-14 15:45:14 +0900

    Initial commit

---
+++ OctoDeck/OctoDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctoDeckApp.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct OctoDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OctoDeckTests/OctoDeckTests.swift
@@ -0,0 +1,17 @@
+//
+//  OctoDeckTests.swift
+//  OctoDeckTests
+//
+//  Created by Kanta Oikawa on 2025/12/14.
+//
+
+import Testing
+@testable import OctoDeck
+
+struct OctoDeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
