5c7abfb906682efda71aa8f92147431cc54afe1d
ryka-1104 <gyd90621@gmail.com>
2025-12-15 13:15:52 +0900

    fix iconUrl and more in Card.swift

--- OctoDeck/Entity/Card.swift
+++ OctoDeck/Entity/Card.swift
@@ -5,30 +5,19 @@
 //  Created by 藤間里緒香 on 2025/12/14.
 //
 
-struct Card: Identifiable, Equatable {
+import Foundation
+
+struct Card: Identifiable {
     let id: String
     let userName: String
     let fullName: String
-    let iconUrl: String
+    let iconUrl: URL?
 }
 extension Card {
     static let stub0 = Card(
         id: "51151242",
         userName: "kantacky",
         fullName: "Kanta Oikawa",
-        iconUrl: "https://avatars.githubusercontent.com/u/51151242?v=4",
-    )
-    //随時追加
-    static let stub1 = Card(
-        id: "",
-        userName: "",
-        fullName: "",
-        iconUrl: "",
-    )
-    static let stub2 = Card(
-        id: "",
-        userName: "",
-        fullName: "",
-        iconUrl: "",
+        iconUrl: URL(string: "https://avatars.githubusercontent.com/u/51151242?v=4")
     )
 }
