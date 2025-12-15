741407e34c390b9c032cf52b5041ea66a9c85641
ryka-1104 <gyd90621@gmail.com>
2025-12-15 13:33:11 +0900

    create CardView.swift

---
+++ OctoDeck/Component/CardView.swift
@@ -0,0 +1,67 @@
+//
+//  CardView.swift
+//  OctoDeck
+//
+//  Created by 藤間里緒香 on 2025/12/15.
+//
+
+import NukeUI
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+
+    var body: some View {
+        HStack {
+            image
+                .frame(width: 80, height: 80)
+                .clipped()
+                .cornerRadius(12)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.secondary.opacity(0.2), lineWidth: 1)
+                }
+            contentView
+        }
+    }
+
+    private var contentView: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(card.userName)
+                .font(.body)
+                .bold()
+                .lineLimit(1)
+                .multilineTextAlignment(.leading)
+
+            Text(card.fullName)
+                .font(.caption)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+        }
+    }
+
+    private var image: some View {
+        LazyImage(url: card.iconUrl) { state in
+            if let image = state.image {
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } else if state.error != nil {
+                defaultImage
+            } else {
+                ProgressView()
+            }
+        }
+    }
+
+    private var defaultImage: some View {
+        Image("default")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+    }
+}
+
+#Preview {
+    CardView(card: .stub0)
+        .padding()
+}
