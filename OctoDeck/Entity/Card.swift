032cdac6623ac67d9682c0b45d71e6a568b55050
ryka-1104 <gyd90621@gmail.com>
2025-12-15 23:03:42 +0900

    add DomainColor about identicon in Card.swift

--- OctoDeck/Entity/Card.swift
+++ OctoDeck/Entity/Card.swift
@@ -18,6 +18,15 @@ struct DomainColor: Decodable, Equatable {
     let b: Int
 }
 
+extension DomainColor {
+    init(id: String) {
+        let hash = abs(id.hashValue)
+        self.r = (hash >> 16) & 0xFF
+        self.g = (hash >> 8) & 0xFF
+        self.b = hash & 0xFF
+    }
+}
+
 struct Card: Identifiable {
     let id: String
     let userName: String
@@ -31,6 +40,6 @@ extension Card {
         userName: "kantacky",
         fullName: "Kanta Oikawa",
         iconUrl: URL(string: "https://avatars.githubusercontent.com/u/51151242?v=4"),
-        identicon: Identicon(color: DomainColor(r: <#T##Int#>, g: <#T##Int#>, b: <#T##Int#>), blocks: [[true]]) // RGBの値を設定
+        identicon: Identicon(color: DomainColor(id: "51151242"), blocks: [[true]])
     )
 }
