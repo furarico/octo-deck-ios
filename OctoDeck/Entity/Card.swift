3a3ffc716cfbe35a4f4cf44eaf72f8b3429c439b
ryka-1104 <gyd90621@gmail.com>
2025-12-15 20:22:08 +0900

    add identicon in Card.swift

--- OctoDeck/Entity/Card.swift
+++ OctoDeck/Entity/Card.swift
@@ -7,17 +7,30 @@
 
 import Foundation
 
+struct Identicon: Decodable {
+    let color: DomainColor
+    let blocks: [[Bool]]
+}
+
+struct DomainColor: Decodable, Equatable {
+    let r: Int
+    let g: Int
+    let b: Int
+}
+
 struct Card: Identifiable {
     let id: String
     let userName: String
     let fullName: String
     let iconUrl: URL?
+    let identicon: Identicon
 }
 extension Card {
     static let stub0 = Card(
         id: "51151242",
         userName: "kantacky",
         fullName: "Kanta Oikawa",
-        iconUrl: URL(string: "https://avatars.githubusercontent.com/u/51151242?v=4")
+        iconUrl: URL(string: "https://avatars.githubusercontent.com/u/51151242?v=4"),
+        identicon: Identicon(color: DomainColor(r: <#T##Int#>, g: <#T##Int#>, b: <#T##Int#>), blocks: [[true]]) // RGBの値を設定
     )
 }
