c67338a18d4fd0d5ff750828c7c763051565179d
Kanta Oikawa <iam@kantacky.com>
2025-12-16 20:36:28 +0900

    Add CardRepository

---
+++ OctoDeck/Repository/CardRepository.swift
@@ -0,0 +1,91 @@
+//
+//  CardRepository.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/16.
+//
+
+import Dependencies
+import DependenciesMacros
+import Foundation
+
+@DependencyClient
+struct CardRepository {
+    var listCards: @Sendable () async throws -> [Card]
+    var getCard: @Sendable (_ id: Card.ID) async throws -> Card
+    var getMyCard: @Sendable () async throws -> Card
+}
+
+extension CardRepository: DependencyKey {
+    static let liveValue = CardRepository(
+        listCards: {
+            let client = try await Client.build()
+            let response = try await client.getCards()
+            switch response {
+            case .ok(let okResponse):
+                let responseCards = try okResponse.body.json.cards
+                return responseCards.map {
+                    Card(
+                        id: $0.id,
+                        userName: $0.userName,
+                        fullName: $0.fullName,
+                        iconUrl: URL(string: $0.iconUrl),
+                        identicon: Identicon(
+                            color: DomainColor(hexCode: $0.identicon.color),
+                            blocks: $0.identicon.blocks
+                        )
+                    )
+                }
+            default:
+                throw CardRepositoryError.failedToFetchCards
+            }
+        },
+        getCard: { id in
+            let client = try await Client.build()
+            let response = try await client.getCard(path: .init(id: id))
+            switch response {
+            case .ok(let okResponse):
+                let responseCard = try okResponse.body.json.card
+                return Card(
+                    id: responseCard.id,
+                    userName: responseCard.userName,
+                    fullName: responseCard.fullName,
+                    iconUrl: URL(string: responseCard.iconUrl),
+                    identicon: Identicon(
+                        color: DomainColor(hexCode: responseCard.identicon.color),
+                        blocks: responseCard.identicon.blocks
+                    )
+                )
+            default:
+                throw CardRepositoryError.failedToFetchCards
+            }
+        },
+        getMyCard: {
+            let client = try await Client.build()
+            let response = try await client.getMyCard()
+            switch response {
+            case .ok(let okResponse):
+                let responseCard = try okResponse.body.json.card
+                return Card(
+                    id: responseCard.id,
+                    userName: responseCard.userName,
+                    fullName: responseCard.fullName,
+                    iconUrl: URL(string: responseCard.iconUrl),
+                    identicon: Identicon(
+                        color: DomainColor(hexCode: responseCard.identicon.color),
+                        blocks: responseCard.identicon.blocks
+                    )
+                )
+            default:
+                throw CardRepositoryError.failedToFetchCards
+            }
+        }
+    )
+}
+
+extension DependencyValues {
+    var cardRepository: CardRepository {
+        get { self[CardRepository.self] }
+        set { self[CardRepository.self] = newValue }
+    }
+}
