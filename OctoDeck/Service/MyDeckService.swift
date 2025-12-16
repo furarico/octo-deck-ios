09c6b67e41411a5e15526e9567fa259f605390e9
Kanta Oikawa <iam@kantacky.com>
2025-12-16 20:37:40 +0900

    Add MyDeckService

---
+++ OctoDeck/Service/MyDeckService.swift
@@ -0,0 +1,16 @@
+//
+//  MyDeckService.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/16.
+//
+
+import Dependencies
+
+final actor MyDeckService {
+    @Dependency(\.cardRepository) private var cardRepository
+
+    func getMyCard() async throws -> Card {
+        try await cardRepository.getMyCard()
+    }
+}
