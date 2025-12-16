86201b09e1527fb873b6f5444aab65b425c91fa0
Kanta Oikawa <iam@kantacky.com>
2025-12-16 20:08:04 +0900

    Add APIClient

--- OctoDeck/Helper/APIClient.swift
+++ OctoDeck/Helper/APIClient.swift
@@ -5,4 +5,16 @@
 //  Created by Kanta Oikawa on 2025/12/16.
 //
 
-import Foundation
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+extension Client {
+    static func build() async throws -> Client {
+        return Client(
+            serverURL: try Servers.Server4.url(),
+            configuration: .init(dateTranscoder: .iso8601WithFractionalSeconds),
+            transport: URLSessionTransport(),
+            middlewares: [APIAuthMiddleware()]
+        )
+    }
+}
