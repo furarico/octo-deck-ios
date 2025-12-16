72c5496521aa9b386334d57822e26fb970444717
Kanta Oikawa <iam@kantacky.com>
2025-12-16 11:46:12 +0900

    Add ContentService

---
+++ OctoDeck/Service/ContentService.swift
@@ -0,0 +1,12 @@
+//
+//  ContentService.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/16.
+//
+
+import Dependencies
+
+final class ContentService {
+    @Dependency(\.gitHubAuthRepository) private var gitHubAuthRepository
+}
