7ed44727e9318dd2ee5ad5325d3c908512f245f8
Kanta Oikawa <iam@kantacky.com>
2025-12-16 11:53:17 +0900

    Add ContentServiceTests

---
+++ OctoDeckTests/Service/ContentServiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  ContentServiceTests.swift
+//  OctoDeckTests
+//
+//  Created by Kanta Oikawa on 2025/12/16.
+//
+
+import Dependencies
+import Testing
+@testable import OctoDeck
+
+struct ContentServiceTests {
+    @Test("SignInが正しく終了する")
+    func testSignInSuccess() async throws {
+    }
+
+    @Test("SignInがエラーをthrowする")
+    func testSignInFailure() async throws {
+    }
+
+    @Test("SignOutが正しく終了する")
+    func testSignOutSuccess() async throws {
+    }
+
+    @Test("SignOutがエラーをthrowする")
+    func testSignOutFailure() async throws {
+    }
+
+    @Test("AccessTokenが正しく返却される")
+    func testGetAccessTokenSuccess() async throws {
+    }
+
+    @Test("AccessTokenが返却されない")
+    func testGetAccessTokenFailure() async throws {
+    }
+}
