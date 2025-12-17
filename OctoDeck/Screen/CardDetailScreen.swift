b3467ac7b3e9b80c81c54633561439188af98c81
Kanta Oikawa <iam@kantacky.com>
2025-12-17 16:57:24 +0900

    Add buttons

--- OctoDeck/Screen/CardDetailScreen.swift
+++ OctoDeck/Screen/CardDetailScreen.swift
@@ -5,6 +5,7 @@
 //  Created by Kanta Oikawa on 2025/12/17.
 //
 
+import Foundation
 import SwiftUI
 
 struct CardDetailScreen: View {
@@ -37,10 +38,38 @@ struct CardDetailScreen: View {
             ProgressView()
         } else if let statistic = viewModel.statistic {
             ScrollView {
+                HStack {
+                    Button(role: .close) {
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.title3)
+                            .padding(8)
+                    }
+                    .buttonStyle(.glass)
+                    .buttonBorderShape(.circle)
+                    Spacer()
+                    shareLink
+                }
             }
         } else {
             Text("No statistic available.")
         }
+    }
+
+    private var shareLink: some View {
+        ShareLink(
+            item: URL(string: "https://octodeck.furari.co/users/\(viewModel.card.id)")!,
+            preview: SharePreview(
+                viewModel.card.fullName
+            )
+        ) {
+            Image(systemName: "square.and.arrow.up")
+                .offset(y: -2)
+                .font(.title3)
+                .padding(4)
+        }
+        .buttonStyle(.glass)
+        .buttonBorderShape(.circle)
     }
 }
 
