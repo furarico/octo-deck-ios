45f8fb836f2bb884ba34beea4c45edfeacc00e99
Kanta Oikawa <iam@kantacky.com>
2025-12-17 16:09:00 +0900

    Add Statistic

---
+++ OctoDeck/Entity/Statistic.swift
@@ -0,0 +1,12 @@
+//
+//  Statistic.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/17.
+//
+
+import Foundation
+
+struct Statistic: Equatable {
+    let contributions: [Contribution]
+}
