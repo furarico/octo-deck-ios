dce2ebbc427a76a3ec103c8e4d41a5bd49de5cac
ryka-1104 <gyd90621@gmail.com>
2025-12-18 14:49:14 +0900

    create LottieView.swift and Completed.swift

---
+++ OctoDeck/Component/CompletedView.swift
@@ -0,0 +1,33 @@
+//
+//  CompletedView.swift
+//  OctoDeck
+//
+//  Created by 藤間里緒香 on 2025/12/18.
+//
+import SwiftUI
+import Lottie
+
+struct CompletedView: View {
+    var body: some View {
+        ZStack{
+            Image("OctoDeckBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+                .overlay {
+                    VStack(spacing: 24) {
+                        LottieView(
+                            name:"OcotoDeckCompleted",
+                        ).frame(width: 256,height: 256)
+                        Text("連携完了")
+                            .font(.largeTitle)
+                            .bold()
+                    }.foregroundStyle(Color.white)
+                }
+        }
+    }
+}
+
+#Preview {
+    CompletedView()
+}
