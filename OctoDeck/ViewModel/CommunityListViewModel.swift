1a818a0e5f4e2944f6850b6226686a5630682c00
Kanta Oikawa <iam@kantacky.com>
2025-12-18 16:35:03 +0900

    feat: Add CommunityListViewModel and tests

---
+++ OctoDeck/ViewModel/CommunityListViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  CommunityListViewModel.swift
+//  OctoDeck
+//
+//  Created by Kanta Oikawa on 2025/12/18.
+//
+
+import Observation
+
+@Observable
+final class CommunityListViewModel {
+    private(set) var communities: [Community] = []
+    private(set) var isLoading: Bool = false
+
+    private let service = CommunityListService()
+
+    func onAppear() async {
+        isLoading = true
+        defer {
+            isLoading = false
+        }
+
+        do {
+            communities = try await service.listCommunities()
+        } catch {
+            print(error)
+        }
+    }
+}
