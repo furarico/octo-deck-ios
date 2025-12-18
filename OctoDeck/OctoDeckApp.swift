7533f8d7c220757a0be30da3fd41c754a0cf0075
ryka-1104 <gyd90621@gmail.com>
2025-12-18 20:38:39 +0900

    create CardIssueView.swift

--- OctoDeck/OctoDeckApp.swift
+++ OctoDeck/OctoDeckApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct OctoDeckApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentScreen()
+            CardIssueView()
+           // ContentScreen()
         }
     }
 }
